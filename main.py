4d64f98f7c751f8d1e8ba40a80a501a621941bc9
jassler <erifetim@gmail.com>
2019-04-09 10:43:02 -0400

    Added argparser for ease of use

--- main.py
+++ main.py
@@ -1,72 +1,66 @@
 import os
 import builtins
+import argparse
 from sys import argv, path
 from importlib import import_module
 
 import testmain
 
-usage = '{} [test|benchmark] <problem number>\n'.format(argv[0])
-if len(argv) < 2:
-    print(usage)
-    exit(1)
+parser = argparse.ArgumentParser(description='Test and benchmark daily problems')
+parser.add_argument('-t', action='store_true', help='Test problem set')
+parser.add_argument('-b', action='store_true', help='Benchmark problem set')
+parser.add_argument('problems', metavar='n', type=int, nargs='+', help='Problem number to test or benchmark')
 
-testing = argv[1].lower() == 'test'
-benchmark = argv[1].lower() == 'benchmark'
+args = parser.parse_args()
 
-if (testing or benchmark) and len(argv) < 3:
-    print(usage)
-    exit(1)
+testing = args.t
+benchmark = args.b
 
-try:
-    number = int(argv[-1])
+def handle_flag(folder: str, testtype: str):
+    '''testtype should be either test or benchmark'''
+
+    if testtype == 'test':
+        casemodule = 'testcases'
+        evalutae = testmain.test
+    
+    elif testtype == 'benchmark':
+        casemodule = 'testbenchmarks'
+        evalutae = testmain.benchmark
+    
+    else:
+        print('Unrecognized flag option "{}"'.format(testtype))
+        return
+    
+    cases = getattr(__import__(casemodule), folder, [])
+    if len(cases) == 0:
+        print('No {} cases under the name of {}. Add tests to testcases.py'.format(testtype, folder))
+        return
+    
+    had_tests = False
+    for file in os.listdir(folder):
+        if file.endswith('.py'):
+            module = import_module('{}.{}'.format(folder, file[:-3]))
+            for _, f in getattr(module, '__dict__').items():
+                
+                if callable(f) and getattr(f, testtype, True):
+                    had_tests = True
+                    evalutae(f, cases)
+    
+    if not had_tests:
+        print('No functions found to {}'.format(testtype))
+
+for number in args.problems:
     folder = 'problem_{:03d}'.format(number)
     if not os.path.isdir(folder):
         print('Problem {} does not exist'.format(folder))
-        print(usage)
         exit(1)
-    
+
     path.insert(0, folder)
 
     ### TEST ###
     if testing:
-        cases = getattr(__import__('testcases'), folder, [])
+        handle_flag(folder, 'test')
 
-        had_tests = False
-        for file in os.listdir(folder):
-            if file.endswith('.py'):
-                module = import_module('{}.{}'.format(folder, file[:-3]))
-                for _, f in getattr(module, '__dict__').items():
-                    
-                    if callable(f) and getattr(f, 'test', True):
-                        had_tests = True
-                        testmain.test(f, cases)
-        
-        if not had_tests:
-            print('No functions found to test')
-            exit(2)
-        
-        exit(0)
-    
     ### BENCHMARK ###
     if benchmark:
-        cases = getattr(__import__('testbenchmarks'), folder, [])
-
-        had_benchmarks = False
-        for file in os.listdir(folder):
-            if file.endswith('.py'):
-                module = import_module('{}.{}'.format(folder, file[:-3]))
-                for _, f in getattr(module, '__dict__').items():
-                    
-                    if callable(f) and getattr(f, 'benchmark', True):
-                        had_benchmarks = True
-                        testmain.benchmark(f, cases)
-        
-        if not had_benchmarks:
-            print('No functions found to benchmark')
-            exit(2)
-        exit(0)
-
-except ValueError as e:
-    print(usage)
-    print('Invalid problem "{}", only enter number'.format(argv[-1]))
-    exit(1)
+        handle_flag(folder, 'benchmark')
